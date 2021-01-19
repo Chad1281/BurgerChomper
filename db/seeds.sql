ec0b8d3fef7d029994088d78704b7ae431cad624
Chad Cook <Charles.Cook28@gmail.com>
2021-01-19 15:54:00 -0600

    created all folder, files, and databases

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cobra Kai Habanero Bacon', );
+INSERT INTO burgers (burger_name, devoured) VALUES ('Miyagi-Do Soft Crab', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Daniel Son Vegan', true);
