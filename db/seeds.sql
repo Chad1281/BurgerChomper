f8348dbe2575c49669f375028808191c1bf28eff
Chad Cook <Charles.Cook28@gmail.com>
2021-01-20 21:17:06 -0600

    uploading updated js files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cobra Kai Habanero Bacon', );
-INSERT INTO burgers (burger_name, devoured) VALUES ('Miyagi-Do Soft Crab', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Daniel Son Vegan', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cobra Kai Habanero Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Miyagi-Do Soft Crab', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Daniel Son Vegan', false);
